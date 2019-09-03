47545c4d07d3f5b4581b128b873727df49303a6d
Monika Tadross <monikk.tadross@gmail.com>
2019-09-03 18:59:32 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFpur = "queen";
+
+
+
+console.log("User flipped " + cardTwo)
+console.log("User flipped " + cardThree)
+
