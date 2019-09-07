1098b5393485684430d52071409d26fe5815f0c5
Monika Tadross <monikk.tadross@gmail.com>
2019-09-07 16:36:07 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,19 @@
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFpur = "queen";
+let cards = ["queen", "queen", "king", "king"];
+
+let cardsInPlay = [];
+let cardOne = cards[1];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped queen")
 
 
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king")
 
-console.log("User flipped " + cardTwo)
-console.log("User flipped " + cardThree)
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+ 	alert ("You find a match!");
+ 	} alert("Sorry, try again")
 
+}
